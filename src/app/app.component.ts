db8b957d567879b220d0449a1000686eaf5c8626
morninng <mr_morninng_star_dash@yahoo.co.jp>
2017-12-25 12:38:06 +0900

    connect with namespace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import * as io from 'socket.io-client';
+
+const SOCKET_URL = 'http://127.0.0.1:3000/';
+const SOCKET_NAMESPACE = 'mixidea';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  socket_io: any;
+
+  constructor() {
+
+    this.socket_io = io.connect(SOCKET_URL + SOCKET_NAMESPACE, (socket) => {
+      socket.join('room-a');
+    });
+
+  }
+
+  send_data(){
+
+
+  }
+
+
+  finalize() {
+    this.socket_io.close();
+
+  }
+
+
+
+
+
 }
